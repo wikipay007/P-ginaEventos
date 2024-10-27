1637a5d046a929528f9785521ce451b41391791c
Kauãn Santos <165726707+wikipay007@users.noreply.github.com>
2024-10-27 16:31:50 -0300

    Muita coisa aconteceu
    Nova seção com dizeres sobre os serviços
    Novas imagens

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const myCarouselElement = document.querySelector('#carouselExampleAutoplaying')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 3000,
-  touch: false
-})
+document.addEventListener("DOMContentLoaded", function() {
+  const myCarouselElement = document.querySelector('#carouselExampleAutoplaying');
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 3000,
+    touch: false
+  });
+});
